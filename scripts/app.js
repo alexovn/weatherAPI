48c442ef01268b5d5e1d4f771c5810383d64dbb0
Nikita Alexov <polarbeararmor@gmail.com>
2022-03-19 14:22:45 +0500

    in-progress: change location modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,25 @@
-const weather = new Weather('London', 'GB');
+const weather = new Weather('Moscow', 'RU');
 
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
-function getWeather() {
+const changeWeatherBtn = document.querySelector('#w-change-btn');
+changeWeatherBtn.addEventListener('click', (e) => {
+  const city = document.querySelector('#city').value;
+  const state = document.querySelector('#state').value;
+
+  weather.changeLocation(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  var myModal = new bootstrap.Modal(document.getElementById('locModal'));
+  myModal.hide();
+});
+
+function getWeather () {
   weather.getWeather()
-  .then(results => ui.paint(results))
-  .catch(err => console.log(err));
+    .then(results => ui.paint(results))
+    .catch(err => console.log(err));
 }
--- scripts/ui.js
+++ scripts/ui.js
@@ -5,7 +5,7 @@ class UI {
     this.location = document.querySelector('#w-location');
     this.desc = document.querySelector('#w-desc');
     this.string = document.querySelector('#w-string');
-    // this.icon = document.querySelector('#w-icon');
+    this.icon = document.querySelector('#w-icon');
     this.details = document.querySelector('#w-details');
     this.humidity = document.querySelector('#w-humidity');
     this.pressure = document.querySelector('#w-pressure');
@@ -14,15 +14,16 @@ class UI {
   }
 
   paint(weather) {
+    const iconUrl = 'http://openweathermap.org/img/wn/' + weather.weather[0].icon + '.png'
+
     this.city.textContent = weather.name;
     this.country.textContent = weather.sys.country;
     this.desc.textContent = weather.weather[0].description;
-    this.string.textContent = weather.main.temp;
-    console.log(weather.weather[0].icon)
-    // this.icon.textContent = weather.weather.icon;
-    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
-    this.feelsLike.textContent = `Feels like: ${weather.main.feels_like}`;
-    this.pressure.textContent = `Pressure: ${weather.main.pressure}`;
-    this.wind.textContent = `Wind: ${weather.wind.speed}`;
+    this.string.innerHTML = weather.main.temp + ' ' + '&#8451;';
+    this.icon.setAttribute('src', iconUrl);
+    this.humidity.innerHTML = `Relative Humidity: ${weather.main.humidity}` + '&#37;';
+    this.feelsLike.innerHTML = `Feels like: ${weather.main.feels_like}` + ' ' + '&#8451;';
+    this.pressure.textContent = `Pressure: ${weather.main.pressure}` + ' ' + 'hPa';
+    this.wind.textContent = `Wind: ${weather.wind.speed}` + ' ' + 'm/s';
   }
 }
